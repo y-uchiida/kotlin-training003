c4c658cff220edd0498ea2450fe7f82cef154a71
y-uchiida <y.uchiida@nirneege.com>
2024-01-30 01:40:58 +0900

    最小構成としてSampleController クラスを作成 Webブラウザにテキストを表示するサンプル

--- src/main/kotlin/com/example/Kotlintraining003/SampleController.kt
+++ src/main/kotlin/com/example/Kotlintraining003/SampleController.kt
@@ -1,4 +1,17 @@
 package com.example.Kotlintraining003
 
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+// SpringBoot で、RESTfull APIのエンドポイントとして扱われるようにする
+// @SpringBootApplication: SpringBoot でリクエストを処理するクラスであることの宣言
+// @TestController: RESTfull なAPIを提供するための機能を実装することの宣言
+@SpringBootApplication
+@RestController
 class SampleController {
+    @GetMapping("/hello") // ディレクトリルート
+    fun getHello(): String {
+        return "Spring Boot on Kotlin!!"
+    }
 }
