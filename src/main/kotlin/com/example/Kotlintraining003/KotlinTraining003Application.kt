723df1b38f0aeef70e1600ba8f928d7cbec1f6d9
y-uchiida <y.uchiida@nirneege.com>
2024-01-30 01:17:14 +0900

    KotlinTraining003Application.kt にコメントを追加

--- src/main/kotlin/com/example/Kotlintraining003/KotlinTraining003Application.kt
+++ src/main/kotlin/com/example/Kotlintraining003/KotlinTraining003Application.kt
@@ -3,9 +3,13 @@ package com.example.Kotlintraining003
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+// @SpringBootApplication アノテーション
+// SpringBootの実行時に最初に呼び出されるクラスを示すためのアノテーション
 @SpringBootApplication
 class KotlinTraining003Application
 
 fun main(args: Array<String>) {
+	// @SpringBootApplication アノテーションがついたクラスの
+	// runApplication() メソッドが、アプリケーションの起点になる
 	runApplication<KotlinTraining003Application>(*args)
 }
