85304eda220ef3d572483221fec0dd50927d36e7
y-uchiida <y.uchiida@nirneege.com>
2024-01-30 01:01:17 +0900

    initial commit

---
+++ src/test/kotlin/com/example/Kotlintraining003/KotlinTraining003ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.Kotlintraining003
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinTraining003ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
