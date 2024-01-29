d0b8b8da5d40a74d5c0a2439e95448d5c4169f87
y-uchiida <y.uchiida@nirneege.com>
2024-01-30 02:03:05 +0900

    SampleController のメソッドに、SampleService クラスを用いる例を実装

--- src/main/kotlin/com/example/Kotlintraining003/SampleController.kt
+++ src/main/kotlin/com/example/Kotlintraining003/SampleController.kt
@@ -1,7 +1,10 @@
 package com.example.Kotlintraining003
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RestController
 
 // SpringBoot で、RESTfull APIのエンドポイントとして扱われるようにする
@@ -9,9 +12,32 @@ import org.springframework.web.bind.annotation.RestController
 // @TestController: RESTfull なAPIを提供するための機能を実装することの宣言
 @SpringBootApplication
 @RestController
-class SampleController {
-    @GetMapping("/hello") // ディレクトリルート
+class SampleController(
+    // @RestController を付けたクラスでは、コンストラクタに@Service をつけたクラスを渡すとDI してくれる
+    val sampleService: SampleService
+) {
+    @GetMapping("/hello") // GET /hello へのアクセスを処理する
     fun getHello(): String {
         return "Spring Boot on Kotlin!!"
+    }
+
+    @GetMapping("/sample") // GET /sample へのアクセスを処理する
+    fun getSample(): String {
+        return sampleService.getSample()
+    }
+
+    @PostMapping("/sample") // POST /sample
+    fun postSample(): String {
+        return sampleService.postSample()
+    }
+
+    @PutMapping("/sample") // PUT /sample
+    fun putSample(): String {
+        return sampleService.putSample()
+    }
+
+    @DeleteMapping("/sample") // DELETE /sample
+    fun deleteSample(): String {
+        return sampleService.deleteSample()
     }
 }
