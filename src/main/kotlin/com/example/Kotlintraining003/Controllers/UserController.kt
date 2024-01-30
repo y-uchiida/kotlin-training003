67fbfbdd4d9135d2d6aff05fafb67240614dc6b0
y-uchiida <y.uchiida@nirneege.com>
2024-01-30 21:51:22 +0900

    UserController に、Userの一覧を取得するエンドポイントを追加

--- src/main/kotlin/com/example/Kotlintraining003/Controllers/UserController.kt
+++ src/main/kotlin/com/example/Kotlintraining003/Controllers/UserController.kt
@@ -1,4 +1,20 @@
 package com.example.Kotlintraining003.Controllers
 
-class UserController {
+import com.example.Kotlintraining003.Entities.User
+import com.example.Kotlintraining003.Repositories.UserRepository
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class UserController (
+    val repository: UserRepository
+) {
+    @GetMapping("/users") // GET /users
+    // User エンティティをリポジトリを通じて取得し、
+    // JSON に変換してレスポンスする
+    fun getUsers(): List<User> {
+        val users = repository.findAll()
+
+        return users
+    }
 }
